2ef90b65bee1944b85b96a40d3c3307bc5246aa8
StellarWind007 <kongming05@qq.com>
2016-11-23 09:38:24 +0800

    Add source codes

---
+++ w3school/helloworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// 这是单行注释
+
+# 这也是单行注释
+
+/*
+这是多行注释块
+它横跨了
+多行
+*/
+
+echo "hello world";
+?>
+
+</body>
+</html>
---
+++ w3school/myvariable.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright: 2016 FireworkStudio
+ *
+ * Author:  Daniel Kong
+ * Date:    2016-11-22
+ */
+$color = "red";
+
+$x=5;
+$y=6;
+$z=$x+$y;
+
+$txt="Hello world!";
+
+echo "My car is " . $color . "<br>";    // php 的变量对大小写敏感
+echo $z . "<br>";
+echo $txt . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
